40238bf400528e5d7cef615496a1fe1deb0b5610
YunLiu <55491388+KumoLiu@users.noreply.github.com>
2025-04-02 23:59:47 +0800

    test coverage
    Signed-off-by: YunLiu <55491388+KumoLiu@users.noreply.github.com>

--- runtests.sh
+++ runtests.sh
@@ -720,7 +720,7 @@ if [ $doUnitTests = true ]
 then
     echo "${separator}${blue}unittests${noColor}"
     torch_validate
-    ${cmdPrefix}${cmd} ./tests/runner.py -p "^(?!test_integration).*(?<!_dist)$"  # excluding integration/dist tests
+    ${cmdPrefix}${cmd} ./tests/runner.py -p "^test_handler_mean*"  # only run handler_mean tests
 fi
 
 # distributed test only
